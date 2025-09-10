1061bd6bfe0e5f15373b23219ffa53393ced4f95
Martin Juhl <m@rtinjuhl.dk>
2025-09-10 11:09:40 +0200

    Detect Red Hat

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -166,7 +166,7 @@ function get_os_version() {
     # armbian uses a minimal shell script replacement for lsb_release with basic
     # parameter parsing that requires the arguments split rather than using -sidrc
     mapfile -t os < <(lsb_release -s -i -d -r -c)
-    if [[ "$(echo os[0] | cut -f1 -d' ')" == "RedHatEnterprise" ]]; then
+    if [[ "$(echo ${os[0]} | cut -f1 -d' ')" == "RedHatEnterprise" ]]; then
         echo "RHEL Detected"
     else
         __os_id="${os[0]}"
@@ -179,7 +179,6 @@ function get_os_version() {
     __os_package_variant="deb"
 
     local error=""
-    echo $__os_id
     case "$__os_id" in
         Raspbian|Debian|Bunsenlabs)
             # get major version (8 instead of 8.0 etc)
